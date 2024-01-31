6a88c3d8bcb690136e88b4fa2500252b52dfa509
Cangoo65 <caneraytekin06@hotmail.com>
2024-01-31 21:07:45 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿string[] aylar = { "Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık" };
+foreach (string ay in aylar)
+{
+    Console.WriteLine(ay);
+}
